4d213eb9d77ec687ed41ba207f1d48a72a4d418f
TANIMUL ISLAM <fuadhasanfhm@gmail.com>
2021-05-16 17:51:09 +0600

    Welcome Screen Design

---
+++ app/src/main/java/com/example/imagetopdf/UI/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.imagetopdf.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.imagetopdf.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
